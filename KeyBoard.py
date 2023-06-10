7fce258ded95e87f10742e2a7b17eb5d6330761f
10Valentin01 <115719978+10Valentin01@users.noreply.github.com>
2023-06-11 04:30:44 +0700

    Update KeyBoard.py

--- KeyBoard.py
+++ KeyBoard.py
@@ -1,5 +1,6 @@
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 
+# Выбор суммы в рублях
 kb_price_rub = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
 btn_50_rub = KeyboardButton('50 рублей')
 btn_200_rub = KeyboardButton('200 рублей')
@@ -9,11 +10,13 @@ btn_close = KeyboardButton('Закрыть')
 btn_change = KeyboardButton('Сменить валюту')
 kb_price_rub.add(btn_50_rub, btn_200_rub, btn_500_rub, btn_1000_rub, btn_change).add(btn_close)
 
+# Выбор валюты
 kb_usd_rub = ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True, resize_keyboard=True)
 btn_usd = KeyboardButton('USD')
 btn_rub = KeyboardButton('RUB')
 kb_usd_rub.add(btn_rub, btn_usd)
 
+# Выбор суммы в долларах
 kb_price_usd = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
 btn_50 = KeyboardButton('50 долларов')
 btn_200 = KeyboardButton('200 долларов')
@@ -23,6 +26,8 @@ btn_close = KeyboardButton('Закрыть')
 btn_change = KeyboardButton('Сменить валюту')
 kb_price_usd.add(btn_50, btn_200, btn_500, btn_1000, btn_change).add(btn_close)
 
+
+# Да или нет
 kb_yes_no = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=True)
 btn_yes = KeyboardButton('Да')
 btn_no = KeyboardButton('Нет')
